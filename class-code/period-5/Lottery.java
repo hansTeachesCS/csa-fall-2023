7cdd3e68b49454d2506e5fb3be09b20e69a0d55d
hans <joshuaehans@gmail.com>
2023-12-18 11:48:17 -0500

    random nums w/ 2 helper methods

---
+++ class-code/period-5/Lottery.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class Lottery {
+    public static void main(String[] args) {
+        System.out.println("powerball simulator!");
+        int[] winningNumbers = new int[5];
+        int winningPower = drawRandom(1, 26);
+
+        for(int i = 0; i < 5; i++) {
+            int whiteBall = drawRandom(1, 69);
+            while(!isValid(winningNumbers, whiteBall)) {
+                whiteBall = drawRandom(1, 69);
+            }
+            winningNumbers[i] = whiteBall;
+        }
+        System.out.println(Arrays.toString(winningNumbers));
+        
+    }
+
+    public static int drawRandom(int min, int max) {
+        return (int)(Math.random() * (max - min + 1) + min);
+    }
+
+    public static boolean isValid(int[] arr, int num) {
+        // is the number in range?
+        if (num < 1 || num > 69) {
+            return false;
+        }
+        // is the number already in the list?
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == num) {
+                return false;
+            }
+        }
+        // if we get here, then it's a valid number
+        return true;
+    }
+}
