ad68a76ad757e9bc114bf7d1b07f128a0eae6833
hans <joshuaehans@gmail.com>
2023-12-19 10:14:53 -0500

    count matches using enhanced for loops

--- class-code/period-3/Lottery.java
+++ class-code/period-3/Lottery.java
@@ -12,7 +12,21 @@ public class Lottery {
             }
             winningNumbers[i] = random;
         }
+        int[] userNumbers = new int[5];
+        int userPower = genRandom(1, 26);
+
+        for(int i = 0; i < userNumbers.length; i++) {
+            int random = genRandom(1, 69);
+            while(!isValid(userNumbers, random)) {
+                random = genRandom(1, 69);
+            }
+            userNumbers[i] = random;
+        }
+
         System.out.println(Arrays.toString(winningNumbers));
+        System.out.println(Arrays.toString(userNumbers));
+
+        System.out.println(countMatches(winningNumbers, userNumbers));
     }
 
     public static boolean isValid(int[] arr, int num) {
@@ -31,5 +45,19 @@ public class Lottery {
 
     public static int genRandom(int min, int max) {
         return (int)(Math.random() * (max - min + 1) + min);
+    }
+
+    public static int countMatches(int[] arr1, int[] arr2) {
+        int count = 0;
+
+        // nested enhanced for loops
+        for(int i : arr1) {
+            for(int j : arr2) {
+                if(i == j) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
