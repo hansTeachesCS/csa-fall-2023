370047550f406ce9ac97e173a27981795ae7f6b8
hans <joshuaehans@gmail.com>
2024-01-03 10:13:20 -0500

    sort with a bug

--- class-code/period-3/LinearSort.java
+++ class-code/period-3/LinearSort.java
@@ -5,5 +5,21 @@ public class LinearSort {
         int[] arr = {2,4,1,7,6,3,5,8};
         System.out.println(Arrays.toString(arr));
 
+        for(int i = 0; i < arr.length; i++) {
+            // assume smallest is in front
+            int minIndex = i;
+            int min = arr[i];
+            for(int j = i+1; j < arr.length; j++) {
+                // System.out.println("i: " + arr[i] + " j: " + arr[j]);
+                if(arr[j] < min) {
+                    minIndex = j;
+                } // end if
+            } // end inner loop
+            // make the swap
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
     } // end main method
 } // end class
