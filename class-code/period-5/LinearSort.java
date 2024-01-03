acd56e99089bdccb4750a937a95bf0625a08daf6
hans <joshuaehans@gmail.com>
2024-01-03 10:30:30 -0500

    starter code

---
+++ class-code/period-5/LinearSort.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class LinearSort {
+    public static void main(String[] args) {
+        int[] arr = {2,4,1,7,6,3,5,8};
+        System.out.println(Arrays.toString(arr));
+
+    } // end main method
+} // end class
