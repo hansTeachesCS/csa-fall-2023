0d83f1e70eae9c245bc51e38ea000205b53223c1
hans <joshuaehans@gmail.com>
2024-01-12 11:55:58 -0500

    first class

--- class-code/period-5/first-class/Main.java
+++ class-code/period-5/first-class/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Student s1 = new Student("Joshua", 12);
+        System.out.println(s1);
+
+        Student s2 = new Student("Franklin", 9);
+        System.out.println(s2);
     }
 }
--- class-code/period-5/first-class/Student.java
+++ class-code/period-5/first-class/Student.java
@@ -1,5 +1,21 @@
 public class Student {
+    // fields, instance variables
+    private String name;
+    private int gradeLevel;
+    // TO DO: add the rest of the fields you want
 
+    
+    // constructor method 
+    public Student(String n, int grade) {
+        name = n;
+        gradeLevel = grade;
+    }
+
+
+    // other methods
+    public String toString() {
+        return name + " is a " + gradeLevel + "th grader.";
+    }
 
     
 }
