b1727872659b9ba799792ca8cdf527fc90b002ea
hans <joshuaehans@gmail.com>
2024-01-12 10:15:37 -0500

    first class

---
+++ class-code/period-3/first-class/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student("Hans");
+        System.out.println(s1);
+
+        Student s2 = new Student("Frank");
+        System.out.println(s2);
+
+        // TO DO: create more students!
+    }
+}
---
+++ class-code/period-3/first-class/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    // fields, instance variables
+    private String name;
+    // YOUR TO DO: add all instance variables
+
+
+    // constructor method
+    public Student(String n) {
+        name = n;
+    }
+
+    // other methods
+    public String toString() {
+        return name + " is the best!!!!";
+    }
+
+    // TO DO: add more behaviors, like calculating average 
+}
