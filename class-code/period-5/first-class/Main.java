2dd832de1da774b2af0fea7b9f205cf7e2291938
hans <joshuaehans@gmail.com>
2024-01-19 11:45:40 -0500

    added a setter

--- class-code/period-5/first-class/Main.java
+++ class-code/period-5/first-class/Main.java
@@ -1,11 +1,20 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Student s1 = new Student("Joshua", 12, new int[]{65, 90, 100}, "123456789");
         System.out.println(s1);
+        System.out.println(Arrays.toString(s1.getScores()));
         System.out.println(s1.calculateAverage());
-        
-        Student s2 = new Student("Franklin", 9, new int[]{90, 90, 91}, "234567890");
-        System.out.println(s2);
-        System.out.println(s2.calculateAverage());
+        s1.addScore(95);
+        System.out.println(Arrays.toString(s1.getScores()));
+        System.out.println(s1.calculateAverage());
+
+        s1.setName("Clementius Reuven");
+        System.out.print(s1);
+
+        // Student s2 = new Student("Franklin", 9, new int[]{90, 90, 91}, "234567890");
+        // System.out.println(s2);
+        // System.out.println(s2.calculateAverage());
     }
 }
--- class-code/period-5/first-class/Student.java
+++ class-code/period-5/first-class/Student.java
@@ -13,6 +13,15 @@ public class Student {
         osis = o;
     }
 
+    // other methods
+    public int[] getScores() {
+        return scores;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
     public double calculateAverage() {
         double sum = 0;
         for(int score : scores) {
@@ -21,7 +30,23 @@ public class Student {
         return sum / scores.length;
     }
 
-    // other methods
+    public void addScore(int score) {
+        // Create a new array of size n+1
+        int n = scores.length;
+        int[] newArray = new int[n+1];
+
+        // Add the n elements of the original array in this array.
+        for(int i = 0; i < n; i++) {
+            newArray[i] = scores[i]; 
+        }
+
+        // Add the new element in the n+1 th position.
+        newArray[n] = score;
+
+        // store the new array to the instance variable.
+        scores = newArray;
+    }
+
     public String toString() {
         return name + "(" + osis + ") is a " + gradeLevel + "th grader.";
     }
