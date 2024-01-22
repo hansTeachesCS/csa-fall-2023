cab77b024258c54389f972d4384cdd2da7df3382
hans <joshuaehans@gmail.com>
2024-01-22 11:49:05 -0500

    overloaded constructor

--- class-code/period-5/first-class/Main.java
+++ class-code/period-5/first-class/Main.java
@@ -10,11 +10,17 @@ public class Main {
         System.out.println(Arrays.toString(s1.getScores()));
         System.out.println(s1.calculateAverage());
 
-        s1.setName("Clementius Reuven");
-        System.out.print(s1);
+        // s1.setName("Clementius Reuven");
+        // System.out.println(s1);
 
         // Student s2 = new Student("Franklin", 9, new int[]{90, 90, 91}, "234567890");
         // System.out.println(s2);
         // System.out.println(s2.calculateAverage());
+
+        Student s3 = new Student("Emmi", 10, "543210987");
+        System.out.println(s3);
+        System.out.println(s3.calculateAverage());
+        s3.addScore(100);
+        System.out.println(s3.calculateAverage());
     }
 }
--- class-code/period-5/first-class/Student.java
+++ class-code/period-5/first-class/Student.java
@@ -13,6 +13,12 @@ public class Student {
         osis = o;
     }
 
+    public Student(String n, int grade, String o) {
+        name = n;
+        gradeLevel = grade;
+        osis = o;
+    }
+
     // other methods
     public int[] getScores() {
         return scores;
@@ -31,20 +37,26 @@ public class Student {
     }
 
     public void addScore(int score) {
-        // Create a new array of size n+1
-        int n = scores.length;
-        int[] newArray = new int[n+1];
-
-        // Add the n elements of the original array in this array.
-        for(int i = 0; i < n; i++) {
-            newArray[i] = scores[i]; 
+        if(scores == null) {
+            scores = new int[1];
+            scores[0] = score;
         }
+        else {
+            // Create a new array of size n+1
+            int n = scores.length;
+            int[] newArray = new int[n+1];
 
-        // Add the new element in the n+1 th position.
-        newArray[n] = score;
+            // Add the n elements of the original array in this array.
+            for(int i = 0; i < n; i++) {
+                newArray[i] = scores[i]; 
+            }
 
-        // store the new array to the instance variable.
-        scores = newArray;
+            // Add the new element in the n+1 th position.
+            newArray[n] = score;
+
+            // store the new array to the instance variable.
+            scores = newArray;
+        } 
     }
 
     public String toString() {
